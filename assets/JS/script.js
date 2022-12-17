2db3f05f35303c531f32d101a0972c7295ee213a
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2022-12-17 05:56:57 -0500

    ; added

---
+++ assets/JS/script.js
@@ -0,0 +1,20 @@
+const startBtn = document.getElementById('start-btn')
+
+startBtn.addEventListener('click', startGame)
+
+
+//need to start the game when start button is clicked
+function startGame(){
+console.log('started')
+}
+
+//need f{x} to go to next  question
+function poseNextQuestion(){
+
+}
+
+//selecting answer
+
+function selectAnswer (){
+
+}
