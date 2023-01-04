0a33cd121a54f1d054693ce406057bde132f344d
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-01-03 19:43:22 -0500

    its src not  scr ];

---
+++ assets/JS/script.js
@@ -0,0 +1,39 @@
+//////hey khi you're doin great im proud of you//////
+//  ${variable}
+var myQuestions = ["what's the difference between '==' and '==='", "_____ is a programming interface for HTML and XML documents","_______ f(x)s are used to create objects in javascript","function Person (name, age, gender) {this.name=name; this.age=age; this.gender=gender;} is an example of a ______ f(x)", "______ f(x)s can only be used as a f(x) expression" ]
+var myAnswers= ["", "DOM", "constructor", "constructor", "arrow"];
+console.log("the fuck?");
+console.log('work?');
+//  when the start button is pressed the quiz and timer begins
+
+
+
+// WHEN I answer a question THEN I am presented with another question
+
+
+
+
+// WHEN I answer a question incorrectly THEN time is subtracted from the clock
+
+
+
+
+
+
+// WHEN all questions are answered or the timer reaches 0 THEN the game is over
+
+
+
+
+
+// THEN I can save my initials and my score
+
+
+
+
+// on click for random button href to video//
+const whatIsThisButton= document.getElementById("whatIsThis");
+whatIsThisButton.addEventListener("click", console.log("works?"));
+
+
+//Start the GAME//
