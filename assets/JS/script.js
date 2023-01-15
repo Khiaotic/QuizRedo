d09f4038ce862a0c3a636e59c409a23855b08bad
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-01-15 08:55:55 -0500

    working with console log habits

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,24 @@
 //////hey khi you're doin great im proud of you//////
-//  ${variable}
+//  ${variables}
 var myQuestions = ["what's the difference between '==' and '==='", "_____ is a programming interface for HTML and XML documents","_______ f(x)s are used to create objects in javascript","function Person (name, age, gender) {this.name=name; this.age=age; this.gender=gender;} is an example of a ______ f(x)", "______ f(x)s can only be used as a f(x) expression" ]
 var myAnswers= ["", "DOM", "constructor", "constructor", "arrow"];
-console.log("the fuck?");
-console.log('work?');
+var wrongAnswers=["facts,'duh',who'","huh, 'stop', "]
+var question =  document.getElementById("❔")
 //  when the start button is pressed the quiz and timer begins
-
+// function startQuiz_Timer (){
+//     if (startBtnPressed) {
+//         console.log('start?');
+//     } else  {
+//         console.log('not start?');
+//     }
+// }
 
 
 // WHEN I answer a question THEN I am presented with another question
-
-
+//if question index 0 and answer index 0 match go to next question
+for (var i = 0; i < myQuestions.length; i++) {
+    console.log('question:' + myQuestions[i]);
+}
 
 
 // WHEN I answer a question incorrectly THEN time is subtracted from the clock
@@ -32,8 +40,11 @@ console.log('work?');
 
 
 // on click for random button href to video//
-const whatIsThisButton= document.getElementById("whatIsThis");
-whatIsThisButton.addEventListener("click", console.log("works?"));
+// function visitPage () {
+//     document.getElementById(whatIsThis).innerHTML;
+
+// const whatIsThisButton= document.getElementById("whatIsThis");
+// whatIsThisButton.addEventListener("click", visitPage);
 
 
 //Start the GAME//
